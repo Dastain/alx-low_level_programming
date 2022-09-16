60ecf5cede50c0cf3de699dd2fb7a38c11962cce
Yakubu Sheriff <inovaterconcept@yahoo.com>
2022-09-16 13:37:40 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/my_mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("welcome\n");
+
+	int a; 
+	int b;
+	int c = a*b;
+	
+	printf("pls input ur digits\n");
+	scanf("%d %d", &a,&b);
+	printf("your answer is:%d", c);
+}
