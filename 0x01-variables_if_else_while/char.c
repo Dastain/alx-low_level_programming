18dfd4dc9e8d814c27500c6e463cb191404c0e31
Yakubu Sheriff <inovaterconcept@yahoo.com>
2022-09-18 07:18:36 -0700

    asd

---
+++ 0x01-variables_if_else_while/char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+int i =34;
+	if(i >= 34){
+	putchar(97);
+	putchar('\n');
+	printf("who re u: %d\n", i);
+	}
+	else
+	{
+		printf("pls input a bigger digit\n");
+	}
+		 }
