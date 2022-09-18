18c8aa6713c42aab7943368b711a987389ec46b9
Yakubu Sheriff <inovaterconcept@yahoo.com>
2022-09-18 07:43:39 -0700

    asd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int i;
+	for (i = 0; i > 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
